b22a5373552f9776204e9c23c73122fff14299f4
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-06 14:14:54 -0500

    Day 1 part 2 works

--- ex01/src/ex01.nim
+++ ex01/src/ex01.nim
@@ -5,8 +5,26 @@ import strutils
 import sugar
 import system/io
 
-func fuelForMass(mass: Natural): Natural =
-  result = (mass / 3).Natural - 2
+func fuelForMass(mass: Natural): int =
+  debugEcho "\tmass: ", mass
+  var fraction = trunc(mass / 3)
+  debugEcho "\tfrac: ", fraction
+  result = fraction.int - 2
+  debugEcho "\tresult: ", result
+
+func includingFuelMass(mass: Natural): Natural =
+  result = 0
+  # When I failed to cast, m was a Natural, and then assignment failed a range check when fuelForMass returned -2.
+  # All this logging was towards understanding that.
+  var m = mass.int
+  debugEcho "initially: ", m
+  while m > 0:
+    m = fuelForMass(m)
+    debugEcho "thence: ", m
+    if m > 0:
+      result += m
+      debugEcho "totaling: ", result
+  debugEcho "lastly totaling: ", result
 
 func test =
   const masses = [12, 14, 1969, 100756]
@@ -17,6 +35,8 @@ func test =
 
 proc main =
   var filename: string
+  # Yeah, OK, this param name makes no sense. Not gonna fix it.
+  var day = 1
 
   # BUG: nimble run ex01 --help intercepts --help rather than passing, in spite of docs.
   # (Also -- is just treated as an empty longopt.)
@@ -25,15 +45,20 @@ proc main =
     case kind
     of cmdArgument:
       filename = key
+    of cmdShortOption:
+      if key == "d":
+        day = val.parseInt
     else:
       echo "invalid arg:", repr((kind: kind, key: key, val: val))
 
   if filename == "":
-    stderr.writeLine("usage: ex01 INPUTPATH")
+    stderr.writeLine("usage: ex01 -d:PART INPUTPATH")
     quit(QuitFailure)
 
+  var massFn = if day == 1: fuelForMass else: includingFuelMass
+
   var masses = toSeq(filename.lines).map(line => parseInt(line).Natural)
-  var fuelNeeded = sum(masses.map(fuelForMass))
+  var fuelNeeded = sum(masses.map(massFn))
   echo(fuelNeeded)
 
 
