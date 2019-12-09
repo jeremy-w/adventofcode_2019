d92d6edce5396f70591ff4bc71872db5939f69c9
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-08 21:00:40 -0500

    Adopt "instruction pointer" terminology
    Previously used "program counter".

--- src/day2.nim
+++ src/day2.nim
@@ -1,33 +1,40 @@
 import strutils, sequtils, strformat
 
 type
+    ## Intcode computer opcodes.
+    ## Day 2: opAdd, opMultiply, opHalt
     Opcode = enum
         opAdd = (1, "+")
         opMultiply = (2, "*")
         opHalt = (99, ".")
 
+## Runs an Intcode program.
+##
+## An Intcode machine's memory starts at address 0.
+## An Opcode has zero or more parameters.
 proc run(program: seq[int]): seq[int] =
     var mem = program
-    var pc = 0
+    var ip = 0
     while true:
-        let op = mem[pc].Opcode
-        inc pc
+        let op = mem[ip].Opcode
+        inc ip
 
         case op
         of opAdd, opMultiply:
-            let leftAddr = mem[pc]
+            let leftAddr = mem[ip]
             let left = mem[leftAddr]
-            inc pc
+            inc ip
 
-            let rightAddr = mem[pc]
+            let rightAddr = mem[ip]
             let right = mem[rightAddr]
-            inc pc
+            inc ip
 
-            let resAddr = mem[pc]
-            inc pc
+            let resAddr = mem[ip]
+            inc ip
 
             let output = if op == opAdd: left + right else: left * right
             mem[resAddr] = output
+
         of opHalt:
             return mem
 
