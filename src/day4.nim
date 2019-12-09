ea5672caf33e355e1cc8f28065955ba1769f0566
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-09 10:47:28 -0500

    Day 4, part 2
    Switched to tracking run length directly. That worked once I realized
    that run length always resets to 1, not 0.
    
    > in range 245182-790572, found valid: 1099 and pair-only valid: 710
    
    Benchmark, using bench now:
    
    ```
    > bench ./day4
    benchmarking ./day4
    time                 869.1 ms   (806.1 ms .. 943.0 ms)
                         0.999 R²   (0.997 R² .. 1.000 R²)
    mean                 890.7 ms   (871.3 ms .. 905.3 ms)
    std dev              19.94 ms   (10.03 ms .. 27.45 ms)
    variance introduced by outliers: 19% (moderately inflated)
    ```
    
    Not bad - I even have it doing both parts back to back, vs just one or
    the other.

--- src/day4.nim
+++ src/day4.nim
@@ -3,29 +3,62 @@ import strformat
 const first = 245182
 const last = 790572
 
-proc isValid(password: int): bool =
+when defined(debug):
+  const debug = echo
+else:
+  template debug(x: varargs[untyped]) = discard
+
+
+proc isValid(password: int, allowingLargeGroups: bool = true): bool =
+  debug &"checking {password} allowingLargeGroups={allowingLargeGroups}"
   let str = $password
   var areTwoTheSame = false
   var prev = str[0]
+  var runLength = 1
+
+  proc checkRunLengthOnChange() =
+    if allowingLargeGroups and runLength >= 2 or not allowingLargeGroups and
+        runLength == 2:
+      debug "\t\tfound a valid run length: ", runLength
+      areTwoTheSame = true
+    runLength = 1
+
   for next in str[1..^1]:
-    areTwoTheSame = areTwoTheSame or (next == prev)
+    debug &"\t{prev}, {next} - runLength {runLength}"
+    if not areTwoTheSame and next == prev:
+      runLength += 1
+    else:
+      checkRunLengthOnChange()
     if next < prev:
+      debug "\tINVALID: disqualified for decreasing!"
       return false
     prev = next
+  checkRunLengthOnChange()
+  debug &"\tvalid? {areTwoTheSame}"
   return areTwoTheSame
 
 when defined(test):
   doAssert isValid(111111), "has pair, no decreasing"
   doAssert not isValid(223450), "has decreasing pair"
   doAssert not isValid(123789), "lacks double"
-  echo "tests passed"
+  echo "tests part 1 passed"
+
+  doAssert isValid(112233, false), "valid with larger groups"
+  doAssert not isValid(123444, false), "triple 4"
+  doAssert isValid(111122, false), "has a single pair still"
   quit(QuitSuccess)
 
 var validCnt = 0
+var pairOnlyValidCnt = 0
 var password = last + 1
 while password >= first:
   dec password
   if password.isValid:
     inc validCnt
+  if password.isValid(allowingLargeGroups = false):
+    inc pairOnlyValidCnt
 
-echo fmt"in range {first}-{last}, found valid: {validCnt}"
+# Let's make sure we don't break the known-working output.
+doAssert validCnt == 1099
+
+echo fmt"in range {first}-{last}, found valid: {validCnt} and pair-only valid: {pairOnlyValidCnt}"
