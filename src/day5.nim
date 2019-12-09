c8e8ca327924a239c526eed513d3ceeb9241fdc6
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-09 14:47:52 -0500

    Prep to run Day 5 part 2
    TIL that nim doesn't barf on bad input:
    
    > running: (op: 5 (invalid data!), params: @[]): @[] => @[]
    
    It just keeps running in the loop, even though the case statement has no
    match.

--- src/day5.nim
+++ src/day5.nim
@@ -1,22 +1,36 @@
 import intcode_machine
+import sequtils
+import strformat
 
 type
-    FixedInputWorld = ref object of World
+    FixedInputWorld* = ref object of World
         inputs*: seq[int]
         outputs*: seq[int]
+        shouldEcho: bool
 
-method onInput(w: FixedInputWorld): int =
+method onInput*(w: FixedInputWorld): int =
     if w.inputs.len > 1:
         w.inputs.pop
     else:
         w.inputs[0]
 
-method onOutput(w: FixedInputWorld, i: int, ip: int, mem: seq[int]) =
+method onOutput*(w: FixedInputWorld, i: int, ip: int, mem: seq[int]) =
     w.outputs.add(i)
-    procCall onOutput(w.World, i, ip, mem)
+    if w.shouldEcho:
+        procCall onOutput(w.World, i, ip, mem)
 
 let diagnosticProgram = readFile("./input/day5.txt").toProgram
+
+echo "part 1:"
 const airConditionerUnitId = 1
 var world = FixedInputWorld(inputs: @[airConditionerUnitId])
-let finalState = diagnosticProgram.run(world = world)
-echo "final state: ", finalState
+discard diagnosticProgram.run(world = world)
+assert world.outputs[0..^2].allIt it == 0
+let part1Answer = world.outputs[^1]
+echo "result: ", part1Answer
+assert part1Answer == 12440243, &"got: {part1Answer}"
+
+echo "\n=====\n\npart 2:"
+const thermalRadiatorControllerId = 5
+var world2 = FixedInputWorld(inputs: @[thermalRadiatorControllerId])
+discard diagnosticProgram.run(world = world2)
