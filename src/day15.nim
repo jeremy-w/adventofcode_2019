10d1b28386562f7653ab1f29f53bcf72c42a33f7
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-17 23:43:43 -0500

    Prep for day15

---
+++ src/day15.nim
@@ -0,0 +1,29 @@
+#[
+Day 15: Maze.
+
+You can't see anything to start with, so you have to discover your environment.
+]#
+import intcode_machine_v2
+
+type
+  MoveCmd = enum
+    mcN = (1, "n")
+    mcS = (2, "s")
+    mcW = (3, "w")
+    mcE = (4, "e")
+
+  StatusReply = enum
+    srHitWall
+    srDidMove
+    srFoundIt
+
+const
+  VarRange = 1032..1044
+  MazeRange = 252..1031
+
+const prog = readFile("input/day15.txt").toProgram
+echo prog.toPrettyProgram(@[VarRange, MazeRange])
+
+#[
+  What is the fewest number of movement commands required to move the repair droid from its starting position to the location of the oxygen system?
+]#
