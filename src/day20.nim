75254ec13f62caace368d86cb3a03969e0946610
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-20 23:19:20 -0500

    Prep for day 20

---
+++ src/day20.nim
@@ -0,0 +1,7 @@
+#[
+  Egads, another maze map thingy. :(
+
+  This is a shortest path puzzle with the twist that you can warp between labeled points. Points have an inner and an outer label. Only . is traversable, and labeled . warps you, except for AA (start) and ZZ (end).
+
+  Read vertically stacked labels with the upper letter first.
+]#
