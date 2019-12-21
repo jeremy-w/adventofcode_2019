53ce922f52389ac4fd2999a86ef2948addce4a9c
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-20 22:52:23 -0500

    Not quite got day 19 part 2

--- src/day19.nim
+++ src/day19.nim
@@ -41,15 +41,90 @@ proc recordResult(i: Int, m: Machine) =
   lastY = scannedPoint.y
   stdout.write(if i > 0: "#" else: ".")
 
-echo &"scanning {points.len} points"
+# echo &"scanning {points.len} points"
 
-while true:
-  let machine = makeMachine(prog, onInput = feedNextCoord,
-      onOutput = recordResult)
-  machine.run()
-  if nextIndex > points.high:
-    break
+# while true:
+#   let machine = makeMachine(prog, onInput = feedNextCoord,
+#       onOutput = recordResult)
+#   machine.run()
+#   if nextIndex > points.high:
+#     break
 
-let pointsAffectedCnt = toSeq(results.values).sum
-echo "points affected: ", pointsAffectedCnt
-assert pointsAffectedCnt == 173
+# let pointsAffectedCnt = toSeq(results.values).sum
+# echo "points affected: ", pointsAffectedCnt
+# assert pointsAffectedCnt == 173
+
+#[
+  Part 2:
+    Find the 100x100 square closest to the emitter that fits entirely within the tractor beam; within that square, find the point closest to the emitter.
+    What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate?
+  ]#
+
+proc scan(point: Point): Int =
+  var sendX = true
+  var theAnswer = 0.Int
+  let machine = makeMachine(prog, onInput = proc (m: Machine): Int =
+    result = if sendX: point.x else: point.y
+    sendX = not sendX,
+    onOutput = proc(i: Int, m: Machine) =
+      theAnswer = i)
+  machine.run
+  return theAnswer
+
+proc scanRow(y: int): Slice[Int] =
+  var x = 0.Int
+  var firstX = -1.Int
+  var lastX = -1.Int
+  while scan((x: x, y: y.Int)) == 0:
+    inc x
+  firstX = x
+  while scan((x: x, y: y.Int)) == 1:
+    lastX = x
+    inc x
+  return firstX..lastX
+
+assert scanRow(11) == 7.Int .. 7.Int
+assert scanRow(12) == 7.Int .. 8.Int
+
+var y = 705
+#[
+while scanRow(y).len < 100:
+  inc y
+echo "first row of width 100 is: ", y
+echo "it covers: ", scanRow(y)
+]#
+# first row of width 100 is: 705
+# it covers: 393 .. 492
+
+proc scanCol(p: Point): Slice[Int] =
+  var firstY = -1.Int
+  var lastY = -1.Int
+  var y = p.y
+  if scan(p) == 1:
+    firstY = y
+    dec y
+    while scan((x: p.x, y: y)) == 1:
+      firstY = y
+      dec y
+    y = p.y
+  else:
+    while scan((x: p.x, y: y)) == 1:
+      inc y
+    firstY = y
+  while scan((x: p.x, y: y)) == 1:
+    lastY = y
+    inc y
+  return firstY..lastY
+
+assert scanCol((x: 4.Int, y: 6.Int)) == 6.Int .. 7.Int
+
+for x in 393.Int .. 492.Int:
+  let p = (x: x, y: y.Int)
+  let covered = scanCol(p)
+  echo &"{p} covers {covered.len}: {covered}"
+# (x: 393, y: 705) covers 143: 563 .. 705
+
+let hit = (x: 393.Int, y: 705.Int)
+echo &"hit: {hit}, answer: {hit.x * 10000 + hit.y}"
+# That's not the right answer; your answer 3930705 is too low.
+# Oh, I bet the width on the far side is bogus.
