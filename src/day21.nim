1631375b67784023d87f5ef11f6c0b017b57bbc5
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-12-21 21:43:05 -0500

    Day 21, part 1 done

--- src/day21.nim
+++ src/day21.nim
@@ -48,9 +48,97 @@ proc printIfAscii(i: Int, m: Machine) =
   else:
     echo &"\p\p*** Non-ASCII output! Hull damage: {i}"
 
+#[
+  Jump if not A:
+
+  jump if not (B or C) and D
+
+  (or
+    (and [not (or b c false)] d)
+    (not a))
+]#
+# fails for: #..#@#
+let try1 = """
+NOT A J
+WALK
+"""
+# fails for what try1 dodges: #.#
+let try2 = """
+OR B J
+OR C J
+NOT J J
+AND D J
+WALK
+"""
+  # fails for: #.#.@#, so the mirror of try1
+  # would be avoided by: a b (not c) d
+let try3 = """
+OR B J
+OR C J
+NOT J J
+AND D J
+NOT A T
+OR T J
+WALK
+"""
+
+# Huh, this made it!
+# *** Non-ASCII output! Hull damage: 19359316
+let try4 = """
+OR B J
+OR C J
+NOT J J
+AND D J
+NOT A T
+OR T J
+NOT C T
+AND A T
+AND B T
+AND D T
+OR T J
+WALK
+"""
+let part1Solution = try4
+
+# Instead of ending your springcode program with WALK, use RUN. Doing this will enable extended sensor mode, capable of sensing ground up to nine tiles away. Use registers EFGHI for 56789.
+
+# Fails the same as try1.
+# I thought maybe running would let it clear more spaces on a jump, but it doesn't. Still just 4.
+let p2try1 = """
+NOT A J
+RUN
+"""
+
+# Yeah, RUN just unlocks more "map".
+#[
+This fails after 2 jumps. It jumps to the X, then falls in.
+  ......@..........
+  #####.X.##.#.####
+
+  #.#.##@#.#
+
+  would be avoided by not jumping on the try4 fix,
+  or probably better: teaching it to recognize this bigger pattern of on/off. but we only have 3 lines left to work with, yikes.
+]#
+let p2try2 = part1Solution.replaceWord("WALK", "RUN")
+
+let p2try3 = """
+OR B J
+OR C J
+NOT J J
+AND D J
+AND I J
+NOT A T
+OR T J
+NOT C T
+AND A T
+AND B T
+AND D T
+OR T J
+RUN
+"""
 let m = makeMachine(
   prog,
-  onInput = feedStream(newStringStream(
-    "NOT A J\nWALK\n")),
+  onInput = feedStream(newStringStream(p2try3)),
   onOutput = printIfAscii)
 m.run()
